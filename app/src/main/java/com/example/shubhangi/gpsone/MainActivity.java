dbbfabb079d8db6ceaf24fd4b54d06bcbdf6e926
Shubhino96 <shubhino96@gmail.com>
2018-04-14 17:04:48 +0530

    Complete app features

--- app/src/main/java/com/example/shubhangi/gpsone/MainActivity.java
+++ app/src/main/java/com/example/shubhangi/gpsone/MainActivity.java
@@ -1,17 +1,24 @@
 package com.example.shubhangi.gpsone;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.ClipData;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Handler;
 import android.os.Message;
+import android.preference.PreferenceManager;
+import android.provider.MediaStore;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -20,38 +27,88 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 public class MainActivity extends AppCompatActivity implements LocationListener {
     TextView tvl,test;
-    Button btn;
+    Button btn,editbtn;
     LocationManager locationManager;
     private static final int MY_PERMISSION_REQUEST_CODE = 7171;
     Location location;
+    EditText ed1;
     double latd,lngd;
+    TextView tv;
+    String setadr=" ";
+    String url="http://app.connect247.co.in:8080/connect247";
+    AuthenticationInput auth;
+    int SELECT_PICTURE=4;
+    public static final MediaType MEDIA_TYPE =
+            MediaType.parse("application/json");
+    MediaType MEDIA_TYPE_IMAGE=MediaType.parse("image/jpg");
+    String imageEncoded;
+    List<String> imagesEncodedList;
+    private final int PICK_IMAGE_MULTIPLE =1;
+    private ArrayList<String> imagesPathList;
 
+
+    final OkHttpClient client=new OkHttpClient();
+    ArrayList<String> ar1=new ArrayList<>();
+    Calendar cal=Calendar.getInstance();
+    SimpleDateFormat sdf=new SimpleDateFormat("yyyy/mm/dd");
+    String str=sdf.format(cal.getTime());
+    Date dte=new Date(str);
+    @SuppressLint("MissingPermission")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 200, 1000,this);
-        Toolbar toolbar =  findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         // Sets the Toolbar to act as the ActionBar for this Activity window.
         // Make sure the toolbar exists in the activity and is not null
         setSupportActionBar(toolbar);
-        tvl=findViewById(R.id.location);
-        test=findViewById(R.id.loctest);
-        btn=findViewById(R.id.locbuttn);
+        tvl=(TextView)findViewById(R.id.location);
+        tv=(TextView)findViewById(R.id.cattxt);
+        test=(TextView)findViewById(R.id.loctest);
+        btn=(Button)findViewById(R.id.locbuttn);
+        editbtn=(Button)findViewById(R.id.set);
+        ed1=(EditText)findViewById(R.id.edit1);
+        editbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tv.setText(ed1.getText().toString());
+            }
+        });
+
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, SampleSubcat.class);
+                Intent intent = new Intent(MainActivity.this,ReportActivity.class);
                 startActivity(intent);
             }
         });
@@ -64,8 +121,11 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
                 latd=location.getLatitude();
                 lngd=location.getLongitude();
                 test.setText(latd+"\n"+lngd);
+                Addhandler ah=new Addhandler();
+
                 String cityName = null;
                 String locality= null;
+                String addr1=null;
                 Geocoder gcd = new Geocoder(getBaseContext(), Locale.getDefault());
                 List<Address> addresses;
                 try {
@@ -73,21 +133,17 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
                     if(addresses.size()>0){
                         cityName = addresses.get(0).getLocality();
                         locality=addresses.get(0).getSubLocality();
+                        addr1=addresses.get(0).getAddressLine(0);
                     }
                 }
                 catch (IOException e) {
                     e.printStackTrace();
                 }
                 tvl.setText(locality+" "+cityName);
+                ed1.setText(addr1);
                 Log.i("loc",latd+" ");
                 Log.i("loc",latd+" ");
             }
-
-        }
-        if(ActivityCompat.checkSelfPermission(MainActivity.this,Manifest.permission.WRITE_SETTINGS)!= PackageManager.PERMISSION_GRANTED){
-            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.WRITE_SETTINGS},MY_PERMISSION_REQUEST_CODE);
-        }
-        else {
 
         }
 
@@ -101,19 +157,23 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
         latd=location.getLatitude();
         lngd=location.getLongitude();
+        Addhandler ah=new Addhandler();
         String cityName = null;
+        String address=null;
         Geocoder gcd = new Geocoder(getBaseContext(), Locale.getDefault());
         List<Address> addresses;
         try {
             addresses=gcd.getFromLocation(latd,lngd,1);
             if(addresses.size()>0){
                 cityName = addresses.get(0).getLocality();
+                address = addresses.get(0).getAddressLine(0);
             }
         }
         catch (IOException e) {
             e.printStackTrace();
         }
-        tvl.setText(cityName);
+        tvl.setText(setadr);
+        ed1.setText(address);
         Log.i("loc",latd+" ");
     }
 
@@ -136,7 +196,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
             case MY_PERMISSION_REQUEST_CODE:
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     getlocation();
-                    Catview();
+
                     break;
                 }
         }
@@ -149,6 +209,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
         latd=location.getLatitude();
         lngd=location.getLongitude();
+        Addhandler ah=new Addhandler();
         String cityName = null;
         String locality= null;
         Geocoder gcd = new Geocoder(getBaseContext(), Locale.getDefault());
@@ -165,14 +226,35 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         }
         tvl.setText(locality+" "+cityName);
     }
-    public void Catview(){
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, SampleSubcat.class);
-                startActivity(intent);
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if(requestCode == PICK_IMAGE_MULTIPLE){
+                imagesPathList = new ArrayList<String>();
+                String[] imagesPath = data.getStringExtra("data").split("\\|");
+                for(int i=0;i<imagesPath.length;i++){
+                    imagesPathList.add(imagesPath[i]);
+                }
+                Toast.makeText(getApplicationContext(),imagesPath[0],Toast.LENGTH_LONG).show();
+                }
             }
-        });
+        }
+
+
+    private class Addhandler extends Handler{
+        @Override
+        public void handleMessage(Message msg) {
+            String locationAddress;
+            switch (msg.what){
+                case 1:
+                    Bundle bundle = msg.getData();
+                    locationAddress = bundle.getString("address");
+                    break;
+                default:
+                    locationAddress=null;
+            }
+            ed1.setText(locationAddress);
+        }
     }
 
 }
--- app/src/main/java/com/example/shubhangi/gpsone/SampleSubcat.java
+++ app/src/main/java/com/example/shubhangi/gpsone/SampleSubcat.java
@@ -4,22 +4,140 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class SampleSubcat extends AppCompatActivity {
     RecyclerView rv;
     List<SubCategory> ls;
+    String url="http://app.connect247.co.in:8080/connect247";
+    public static final MediaType MEDIA_TYPE =
+            MediaType.parse("application/json");
+    final OkHttpClient client=new OkHttpClient();
+    ArrayList<Category> catLevel1=new ArrayList<>();
+    ArrayList<Category> catLevel2=new ArrayList<>();
+    ArrayList<Category> catLevel3=new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sample_subcat);
-        rv=findViewById(R.id.recyclerView);
+        rv=(RecyclerView)findViewById(R.id.recyclerView);
         rv.setHasFixedSize(true);
         rv.setLayoutManager(new LinearLayoutManager(this));
         ls=new ArrayList<>();
+        final ArrayList<Category> categories=new ArrayList<>();
+        final Request request = new Request.Builder()
+                .url(url + "/ws/category/list")
+                .get()
+                .header("Content-Type", "application/json")
+                .build();
+        try {
+            Log.i("resp6", "r32");
+            client.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+                    String mMessage = e.getMessage().toString();
+                    Log.i("msk", mMessage);
+                }
+
+                @Override
+                public void onResponse(Call call, final Response response) throws IOException {
+                    final String mMessage = response.body().string();
+                    JSONArray ja = null;
+                    try {
+                        ja = new JSONArray(mMessage);
+                        for (int i = 0; i < ja.length(); i++) {
+                            JSONObject catobj = ja.getJSONObject(i);
+                            String name = catobj.getString("categoryName");
+                            String image = catobj.getString("imageURL");
+                            boolean active = catobj.getBoolean("isActive");
+                            int parent = catobj.getInt("parentCategory");
+                            int id = catobj.getInt("categogyId");
+                            Category c = new Category(id, name, image, active, parent);
+                            JSONArray cat1 = catobj.getJSONArray("categoryList");
+                            if (cat1 != null) {
+                                ArrayList<Category> cl = new ArrayList<>();
+                                for (int j = 0; j < cat1.length(); j++) {
+                                    JSONObject obj = cat1.getJSONObject(j);
+                                    String n = obj.getString("categoryName");
+                                    int id1 = obj.getInt("categogyId");
+                                    String url = obj.getString("imageURL");
+                                    boolean act = obj.getBoolean("isActive");
+                                    int p = obj.getInt("parentCategory");
+                                    Category c1 = new Category(id1, n, url, act, p);
+                                    cl.add(c1);
+                                    c1.setCat(null);
+                                    c1.setSubCategoryClassificationList(null);
+                                    catLevel1.add(c1);
+                                    c.setCat(cl);
+                                    JSONArray cat2 = obj.getJSONArray("subCategoryList");
+                                    if (cat2 != null) {
+                                        ArrayList<Category> c2 = new ArrayList<>();
+                                        for (int k = 0; k < cat2.length(); k++) {
+                                            JSONObject object = cat2.getJSONObject(k);
+                                            String na = object.getString("categoryName");
+                                            int id2 = object.getInt("categogyId");
+                                            String ur = object.getString("imageURL");
+                                            boolean acti = object.getBoolean("isActive");
+                                            int par = object.getInt("parentCategory");
+                                            Category ca2 = new Category(id2, na, ur, acti, par);
+                                            c2.add(ca2);
+                                            ca2.setCat(null);
+                                            ca2.setSubCategoryList(null);
+                                            c1.setSubCategoryList(c2);
+                                            catLevel2.add(ca2);
+                                            JSONArray cat3 = object.getJSONArray("subCategoryClassificationList");
+                                            if (cat3 != null) {
+                                                ArrayList<Category> c3 = new ArrayList<>();
+                                                for (int l = 0; l < cat3.length(); l++) {
+                                                    JSONObject ob = cat3.getJSONObject(l);
+                                                    String nam = ob.getString("categoryName");
+                                                    Log.i("n", nam);
+                                                    int id3 = ob.getInt("categogyId");
+                                                    String iurl = ob.getString("imageURL");
+                                                    boolean iacti = ob.getBoolean("isActive");
+                                                    int pa = ob.getInt("parentCategory");
+                                                    Category ca3 = new Category(id3, nam, iurl, iacti, pa);
+                                                    c3.add(ca3);
+                                                    ca3.setCat(null);
+                                                    ca3.setSubCategoryList(null);
+                                                    ca3.setSubCategoryClassificationList(null);
+                                                    catLevel3.add(ca3);
+                                                    ca2.setSubCategoryClassificationList(c3);
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                            c.setSubCategoryList(null);
+                            c.setSubCategoryClassificationList(null);
+                            categories.add(c);
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
         ls.add(new SubCategory("Cooks",R.drawable.cooks));
         ls.add(new SubCategory("AC Service and Repair",R.drawable.ac_service_and_repair));
         SubCategoryAdapter subadapter=new SubCategoryAdapter(ls);
